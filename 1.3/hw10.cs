72f4d5e2bf1c23d7173daf4c1d78218f990a19fd
Ekaterina Kulazhenkova <kulazhenkova.k@gmail.com>
2022-08-22 21:11:45 +0300

    task 10

---
+++ 1.3/hw10.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string stringNumbers = Convert.ToString(numbers);
+Console.WriteLine("Вторая цифра введенного числа "+ stringNumbers[1]); 
