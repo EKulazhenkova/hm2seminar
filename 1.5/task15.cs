afa20eb2b262e50f50b8fefce2fc26b148f9c5fe
Ekaterina Kulazhenkova <kulazhenkova.k@gmail.com>
2022-08-22 21:12:53 +0300

    task 15

---
+++ 1.5/task15.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите, соответсвующую дню недели ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+    Console.WriteLine("Да,это выходной");    
+    }
+    else if(dayNumber < 1 || dayNumber > 7)
+      {
+       Console.WriteLine("Введено не верное число");
+      }
+    else Console.WriteLine("Нет, это будний день");  
+ 
