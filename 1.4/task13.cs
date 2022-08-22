3c90b539c52af3e7e2461f52eab66b693c328125
Ekaterina Kulazhenkova <kulazhenkova.k@gmail.com>
2022-08-22 21:12:14 +0300

    task 13

---
+++ 1.4/task13.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string stringNumbers = Convert.ToString(numbers);
+if (stringNumbers.Length > 2)
+{
+    Console.WriteLine("Третья цифра введенного числа "+ stringNumbers[2]);    
+}
+else Console.WriteLine("Третьей цифры нет"); 
